d38b56d0ef959fbbab8565e48496916e64628130
vsaran93 <vishnusaran37@gmail.com>
2021-09-04 02:43:34 +0530

    installed entity framework and user modal, migrations added

---
+++ MyClassroom/Models/ClassroomContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyClassroom.Models
+{
+    public class ClassroomContext: DbContext
+    {
+        public ClassroomContext (DbContextOptions options)
+            :base(options) 
+        { 
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
